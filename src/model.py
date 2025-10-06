5a0a6ac39e211fd1b75242d5b23b8fc732062a1b
Reimhagen <kradit26@student.ub.ac.id>
2025-10-06 07:20:54 +0700

    Fixing model.py dan ci_cd_ml.yml

--- src/model.py
+++ src/model.py
@@ -1,13 +1,16 @@
 import sys
 import time
-import os
+
 
 def train_model():
     """Simulasi pelatihan model."""
+
+
+
     print("--- Memulai Pelatihan Model ---")
     time.sleep(2) # Simulasi proses yang memakan waktu
     
-    # Metrik sederhana (simulasi)
+    # Metrik sederhana simulasi
     accuracy = 0.85 
     print(f"Model berhasil dilatih. Akurasi: {accuracy}")
     
@@ -17,9 +20,15 @@ def train_model():
 
     print("--- Pelatihan Model Selesai ---")
 
+
+
 if __name__ == '__main__':
+
+
+
     # Memastikan skrip dipanggil dengan argumen 'train'
     if len(sys.argv) > 1 and sys.argv[1] == 'train':
         train_model()
     else:
         print("Panggil skrip ini dengan 'python src/model.py train'")
+        
